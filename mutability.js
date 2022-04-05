a04f899d474e4a7d91228c5cfcd189ab672c202e
Solexi <sophia.ezeh@pauedu.ng>
2022-04-05 09:38:17 +0100

    modified

--- mutability.js
+++ mutability.js
@@ -1,6 +1,29 @@
-a = 13; //Reassign a new value of type number to a. 
+/*a = 13; //Reassign a new value of type number to a. 
 console.log(a); //Prints out 13. 
 a = "Another value which is a string"; //Reassign a new value of type string to a. 
 console.log(a); //Prints out Another value which is a string. 
 a = true; //Reassign a new value of type boolean - i.e. true or false. 
-console.log(a); //Prints out true
+console.log(a); //Prints out true*/
+
+a = 10; //Assign variable a the value of 10 
+b = 20;//Assign variable b the value of 20 
+c = a + b; //Add value in a and b and put the result in variable c.
+console.log(a<b);
+console.log(c); //This should print out 30 
+console.log(b - a); //Subtract a value from b value; displays 10 in console. 
+console.log(b/a); //Divide b value by a value; displays 2 in console. 
+console.log(a*b); //Multiply a value by b value; displays 200 in console. 
+console.log(b%a); //Divide b value by a value and return remainder; displays 0 in console. 
+console.log(a**3); //a value raised to power 2; displays 100 in console. 
+a++; //Increment a value. 
+console.log(a); //Displays 11 in console. 
+b--; //Increment b value. 
+console.log(b); //Displays 19 in console.
+var age = 17; 
+var adult = (age > 20)? "Yes" : "No"; 
+console.log(adult); //This should display the string "No" 
+
+firstName = "Sophia"; 
+lastName = "Ezeh"; 
+fullName = firstName + " " + lastName; //Concatenate firstName, whitespace and lastName 
+console.log(fullName); //This should output the full name Pius Onobhayedo. 
